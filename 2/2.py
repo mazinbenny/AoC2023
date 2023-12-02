537aa5914ff226e843aac1deb45423d7a7521a22
Benjamin Hess <bghess@umich.edu>
2023-12-01 23:15:21 -0700

    got the regexes working

--- 2/2.py
+++ 2/2.py
@@ -8,12 +8,20 @@ def main():
     progName = "Day2"
 
     #which games possible with 12 red, 13 green, 14 blue
+
+    valid_total = 0
+
     for line in sys.stdin:
         gamelist = line.strip().split(':')
         gamelist[0] = int(re.sub(r'Game ([0-9]+)',r'\1',gamelist[0]))
         gamelist[1] = gamelist[1].split(';')
-        print(gamelist)
-        print(len(gamelist[1]))
+        for check in range(len(gamelist[1])):
+            #if match blue, blue count, else 0
+            if re.search('.*[0-9]+ blue.*',gamelist[1][check]):
+                blue = int(re.sub(r'.*([0-9]+) blue.*',r'\1',gamelist[1][check]))
+            else:
+                blue = 0
+            print(gamelist[1][check]," ###BLUE: ",blue)
 
     return
 

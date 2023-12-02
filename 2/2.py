db3336890401e25e11806e333dd120db5433d860
Benjamin Hess <bghess@umich.edu>
2023-12-01 23:37:55 -0700

    my regex was too greedy with .*, needed to add [ ,] in front of [0-9] -- and this finished the first part of puzzle 2

--- 2/2.py
+++ 2/2.py
@@ -7,21 +7,41 @@ def main():
 
     progName = "Day2"
 
-    #which games possible with 12 red, 13 green, 14 blue
-
     valid_total = 0
 
     for line in sys.stdin:
         gamelist = line.strip().split(':')
         gamelist[0] = int(re.sub(r'Game ([0-9]+)',r'\1',gamelist[0]))
         gamelist[1] = gamelist[1].split(';')
+        valid = True
         for check in range(len(gamelist[1])):
             #if match blue, blue count, else 0
-            if re.search('.*[0-9]+ blue.*',gamelist[1][check]):
-                blue = int(re.sub(r'.*([0-9]+) blue.*',r'\1',gamelist[1][check]))
+            if re.search('.*[, ][0-9]+ blue.*',gamelist[1][check]):
+                blue = int(re.sub(r'.*[, ]([0-9]+) blue.*',r'\1',gamelist[1][check]))
             else:
                 blue = 0
-            print(gamelist[1][check]," ###BLUE: ",blue)
+            if re.search('.*[, ][0-9]+ red.*',gamelist[1][check]):
+                red = int(re.sub(r'.*[, ]([0-9]+) red.*',r'\1',gamelist[1][check]))
+            else:
+                red = 0
+            if re.search('.*[, ][0-9]+ green.*',gamelist[1][check]):
+                green = int(re.sub(r'.*[, ]([0-9]+) green.*',r'\1',gamelist[1][check]))
+            else:
+                green = 0
+
+            #which games possible with 12 red, 13 green, 14 blue
+            #print("red: ",red," green: ",green," blue: ",blue)
+            if red > 12 or green > 13 or blue > 14:
+                valid = False
+                break
+        #after inner loop
+        if valid:
+            #print(gamelist)
+            valid_total += gamelist[0]
+
+
+    print("Total of valid game IDs:")
+    print(valid_total)
 
     return
 

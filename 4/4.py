3045a1c527be314947a3ee43872698ffffcaf822
Benjamin Hess <bghess@umich.edu>
2023-12-03 22:25:02 -0700

    initial take at day 4, example working

---
+++ 4/4.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/python3
+
+import sys
+import re
+
+def main():
+
+    progName = "Day4"
+    total = 0
+
+    data = []
+    for line in sys.stdin:
+        data.append(line.strip())
+
+    for linenum in range(len(data)):
+        nolabel = re.sub(r'Card \d+: ','',data[linenum])
+        nums = nolabel.split('|')
+        win = set(re.findall(r'\d+',nums[0]))
+        mine = set(re.findall(r'\d+',nums[1]))
+        if len(mine.intersection(win)) > 0:
+            total += 2**(len(mine.intersection(win)) - 1)
+
+    print("Total:")
+    print(total)
+
+    return
+
+if __name__ == '__main__':
+    main()
